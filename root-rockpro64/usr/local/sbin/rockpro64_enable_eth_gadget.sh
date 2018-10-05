0c13cf09057a6e753cd03a270e304e99b691ff4a
Kamil Trzciński <ayufan@ayufan.eu>
2018-10-05 21:56:08 +0200

    Add support for usb gadgets for Rock64 and RockPro64

--- root-rockpro64/usr/local/sbin/rockpro64_enable_eth_gadget.sh
+++ root-rockpro64/usr/local/sbin/rockpro64_enable_eth_gadget.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-set -x
+set -xe
 
 # enable peripheral mode
-disable_dtoverlay dwc3_peripheral
+enable_dtoverlay usb0_dwc3_peripheral usb0/dwc3@fe800000 okay \
+  'dr_mode="peripheral"'
 
 # reload dwc3
 echo fe800000.dwc3 > /sys/bus/platform/drivers/dwc3/unbind
 echo fe800000.dwc3 > /sys/bus/platform/drivers/dwc3/bind
 
 # install eth gadget
-uninstall_gadgets
+install_gadget RockPro64 fe800000.dwc3 ecm
