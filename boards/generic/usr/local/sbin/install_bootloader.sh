7ef2e604acf74c76cfcf37287c01aae80257e794
Kamil Trzcinski <ayufan@ayufan.eu>
2026-05-31 10:22:19 +0200

    install_bootloader: detect board from /etc/board-package and installed u-boot package

--- boards/generic/usr/local/sbin/install_bootloader.sh
+++ boards/generic/usr/local/sbin/install_bootloader.sh
@@ -5,9 +5,19 @@ set -eo pipefail
 if [[ -e /etc/board-package ]]; then
   source /etc/board-package
 fi
-if [[ -z "$BOARD" ]]; then
-  echo "The BOARD= is not defined in /etc/board-package"
+
+BOARD_NAMES=()
+[[ -n "$BOARD" ]] && BOARD_NAMES+=("$BOARD")
+
+_pkg_board=$(dpkg -l 'u-boot-*' 2>/dev/null | awk '/^ii/ {print $2}' | sed 's/^u-boot-//' | head -1)
+[[ -n "$_pkg_board" && "$_pkg_board" != "$BOARD" ]] && BOARD_NAMES+=("$_pkg_board")
+
+if [[ "${#BOARD_NAMES[@]}" -eq 0 ]]; then
+  echo "Could not detect board from /etc/board-package or installed u-boot package"
+  exit 1
 fi
+
+BOARD_PATTERN=$(IFS='|'; echo "${BOARD_NAMES[*]}")
 
 if [[ "$#" -ne 1 ]]; then
   echo "Usage: $0 [latest] [u-boot-version]"
@@ -20,7 +30,7 @@ find() {
   fi
   curl --silent --fail "https://api.github.com/repos/ayufan-rock64/$1/releases/$2" \
     | jq -r '.assets | .[] | .browser_download_url' \
-    | grep -E "u-boot-$BOARD-"
+    | grep -E "u-boot-($BOARD_PATTERN)-"
 }
 
 if FILES=$(find linux-mainline-u-boot "$1"); then
@@ -28,7 +38,7 @@ if FILES=$(find linux-mainline-u-boot "$1"); then
   echo "$FILES"
   install_deb.sh $FILES
 else
-  echo "Did not find u-boot '$1' in:"
+  echo "Did not find u-boot '$1' for board(s) [${BOARD_NAMES[*]}] in:"
   echo "- https://github.com/ayufan-rock64/linux-mainline-u-boot/releases"
   exit 1
 fi
